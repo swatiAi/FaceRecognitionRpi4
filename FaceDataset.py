ce564def735d8458b0e3e76a9ccd7daadffa6c32
Usman Swati <usmansawati@gmail.com>
2024-01-15 01:14:10 +0500

    Copyright Edit

--- FaceDataset.py
+++ FaceDataset.py
@@ -1,3 +1,23 @@
+# Author: Usman Javed Swati
+# GitHub: swatiAi
+# Date: 15/01/2024
+#
+# Copyright (c) 2024 Usman Swati
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+# the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+# THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import cv2
 import os
 from picamera2 import Picamera2
@@ -14,22 +34,24 @@ camera.preview_configuration.align()
 camera.configure("preview")
 camera.start()
 
+
 def capture_and_save(frame, user_id, capture_count):
     # Conversion from Color to Greyscale
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Detection of Faces from Greyscale Image Using Haar Classifier
     faces = face_detector.detectMultiScale(
-    frame_gray,      # Analyzing the grayscale image for facial features
-    scaleFactor=1.1,  # Modifying the image scale by 10% at each iteration for improved detection
-    minNeighbors=5,  # Specifying the minimum neighbors required for retaining a candidate rectangle
-    minSize=(30, 30)  # Defining the minimum size of objects; smaller objects are disregarded
+        frame_gray,  # Analyzing the grayscale image for facial features
+        scaleFactor=1.1,  # Modifying the image scale by 10% at each iteration for improved detection
+        minNeighbors=5,  # Specifying the minimum neighbors required for retaining a candidate rectangle
+        minSize=(30, 30)  # Defining the minimum size of objects; smaller objects are disregarded
     )
 
     for (x, y, w, h) in faces:
         # Create an outline around the detected face
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 255), 3) #5 parameters - frame, topleftcoords,bottomrightcooords,boxcolor,thickness
-        
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 255),
+                      3)  # 5 parameters - frame, topleftcoords,bottomrightcooords,boxcolor,thickness
+
         capture_count += 1  # For limiting the image capture
 
         # Save the resized greyscale face to 'dataset' folder
@@ -40,16 +62,17 @@ def capture_and_save(frame, user_id, capture_count):
 
     # Display the image counter
     cv2.putText(frame, f'Count: {capture_count}', (30, 60), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (255, 255, 255), 3)
-    
+
     exit_text = "Press 'q' To Exit"
     exit_text_size = cv2.getTextSize(exit_text, cv2.FONT_HERSHEY_SIMPLEX, 1, 1)[0]
     exit_text_pos = (frame.shape[1] - exit_text_size[0] - 10, frame.shape[0] - 10)
     cv2.putText(frame, exit_text, exit_text_pos, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1)
-    
+
     # Display the frame to the user
     cv2.imshow('YourFace', frame)
 
     return capture_count
+
 
 def main():
     capture_count = 0
@@ -71,13 +94,14 @@ def main():
         if key == 113:  # 'q' key
             break
         elif capture_count >= 10:
-            print(f"\nYour Face ID : {user_id}.") 
+            print(f"\nYour Face ID : {user_id}.")
             print("\nThe Process Is Complete ! Thank You For Your Time :) ")
             break
 
     # Cleanup
     camera.stop()
     cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     main()
